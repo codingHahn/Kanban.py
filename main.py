a30befe50ec5206510a446dc5ad00c921b5477d4
codingHahn <nick.hahn@hotmail.de>
2017-10-25 16:05:42 +0200

    Comment Code Closes #3

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+# Import web.py, the Microframework I am using, hashlib and time for hashing the 'tasks' and tinydb as database
 import web
 import hashlib
 import time
 from tinydb import TinyDB, Query, where
 
+# Declarations for modules
 db = TinyDB('db.json')
 render = web.template.render('templates/', base='layout')
 
+# Declare file structure of the application
 urls = ("/", "index",
         "/add/", "addTask",
         "/delete/*", "deleteTask",
@@ -14,7 +17,9 @@ urls = ("/", "index",
 app = web.application(urls, globals())
 # web.config.debug = True
 
+# Gets called when site is first opened
 class index:
+    """Returns tasks from db"""
     def GET(self):
         section1 = db.search(where('section') == 1)
         section2 = db.search(where('section') == 2)
@@ -28,20 +33,35 @@ class addTask:
             web.form.Textbox("assignee", web.form.notnull, description="Assignee" , class_="validate"),
             web.form.Button("Add To-Do", class_="btn waves-effect waves-light")
             )
+
     def GET(self):
         f = self.form()
         return render.register(f)
+
     def POST(self):
         f= self.form()
         if not f.validates():
-            return render.register(f)
-        hash = hashlib.sha1(f.d.title.encode('utf-8') + f.d.description.encode('utf-8') + f.d.assignee.encode('utf-8') + str(time.time()).encode('utf-8')).hexdigest()
+            return render.register(f)   # TODO: Return Error Message
+        # sha1 is used, because it is fast and this is not security oriented
+        
+        hash = hashlib.sha1(f.d.title.encode('utf-8') +                         # Using the information here to make each UID unique
+                            f.d.description.encode('utf-8') + 
+                            f.d.assignee.encode('utf-8') + 
+                            str(time.time()).encode('utf-8')                    # The hash is 100% unique, because Unix-Time gets factored in
+                           ).hexdigest()
+        # Save task to db
         db.insert({'title':f.d.title, 'description':f.d.description, 'assignee':f.d.assignee, 'hash':hash, 'section':1})
         raise web.seeother('/')
 
+        
 class deleteTask:
     def GET(self):
         url_args = web.input()
+        
+        # This requires every hash to be unique, that is not the case if two task with the same content 
+        # created at exactly the same time. This is a small scale application, so that shouldn't be the case
+        # If it is the case, then both entries get removed on deletion
+        
         db.remove(where('hash') == url_args.hash)
         raise web.seeother('/')
 
@@ -54,6 +74,7 @@ class moveTask:
     def GET(self):
         f = self.form()
         return render.move(f)
+
     def POST(self):
         f = self.form()
         url_args = web.input()
